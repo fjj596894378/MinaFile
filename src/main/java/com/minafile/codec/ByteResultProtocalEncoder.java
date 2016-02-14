7e28d1201733d1185709dfb45ffc42620af371df
fujingjing <596894378@qq.com>
2016-02-14 17:46:06 +0800

    2.0版本最后一次提交。修改客户端，修改服务器。支持多文件同时上传 Signed-off-by: fujingjing <596894378@qq.com>

--- src/main/java/com/minafile/codec/ByteResultProtocalEncoder.java
+++ src/main/java/com/minafile/codec/ByteResultProtocalEncoder.java
@@ -26,16 +26,16 @@ public class ByteResultProtocalEncoder extends ProtocolEncoderAdapter{
 		CharsetEncoder encoder = Charset.forName("UTF-8").newEncoder();// 以UTF-8的方式进行解析字符串
 		// 对message进行强转
 		ByteReturnFileMessage returnMassage = (ByteReturnFileMessage)message;
-		IoBuffer ioBuffer = IoBuffer.allocate( 4 + 4 + returnMassage.getReturnMassage().length()).setAutoExpand(true);
+		IoBuffer ioBuffer = IoBuffer.allocate( 4 + 4 /*+ returnMassage.getReturnMassage().length()*/).setAutoExpand(true);
 		ioBuffer.putInt(returnMassage.getSeq()); // 序号
 		// 传给客户端消息的字节数。
 		// 这里要用到字节数。不过只是获取字符串的长度，那么不对的。
 		// 因为这是根据字节数获取数据的。所以如果字符串中有中文，那么就会获取出错。
-		ioBuffer.putInt(returnMassage.getReturnMassageLength());
+		 ioBuffer.putInt(returnMassage.getReturnMassage()); 
 		
-		LOGGER.info("服务器传给客户端字符长度(字节)：" + returnMassage.getReturnMassageLength() );
-		ioBuffer.putString(returnMassage.getReturnMassage(), encoder);
-		LOGGER.info("服务器准备发送的信息【" + returnMassage.getReturnMassage()+ "】；字符串编码类型：" + encoder.toString());
+		/*LOGGER.info("服务器传给客户端字符长度(字节)：" + returnMassage.getReturnMassageLength() );*/
+		/*ioBuffer.putString(returnMassage.getReturnMassage(), encoder);*/
+		LOGGER.info("服务器准备发送的信息【" + (returnMassage.getReturnMassage() == 1?"成功":returnMassage.getReturnMassage() == 2?"失败":"其它")+ "】；字符串编码类型：" + encoder.toString());
 		ioBuffer.flip();
 		LOGGER.info("服务器编码完成");
 		out.write(ioBuffer);
