f65af1e37e7974eac5f963cf44dddc4388de13af
fujingjing <596894378@qq.com>
2016-02-14 10:14:31 +0800

    1.准备加版本 2.0 Signed-off-by: fujingjing <596894378@qq.com>

--- src/main/java/com/minafile/codec/ByteProtocalDecoder.java
+++ src/main/java/com/minafile/codec/ByteProtocalDecoder.java
@@ -16,7 +16,7 @@ import com.minafile.model.ByteFileMessage;
 /**
  * 这个是服务器对客户端发来的消息进行解码 继承CumulativeProtocolDecoder 实现doDecode
  * 父类会将数据读取完之后，再调用实现的方法doDecode。 如果成功读取完之后，服务器会去Handle中进行业务处理。
- * 对发来的文件进行业务处理，比如说保存之类的 动作。
+ * 对发来的文件进行业务处理，比如说保存之类的 动作。 1. 2016.02.14 实现多文件粘包
  * 
  * @author king_fu
  * 
@@ -45,7 +45,6 @@ public class ByteProtocalDecoder extends CumulativeProtocolDecoder {
 			// 这个方法的调用是判断IoBuffer里的数据是否满足一条消息了
 			// dataLength = getInt(position()); 用绝对值的方式读取，position不会移动。
 
-			/* if ((!isFirst) || in.prefixedDataAvailable(4, MAX_FILE_SIZE)) { */
 			this.readFile(in);
 			if (isFinish) {
 				// 解析完成
@@ -64,68 +63,37 @@ public class ByteProtocalDecoder extends CumulativeProtocolDecoder {
 	private void readFile(IoBuffer in) throws CharacterCodingException {
 		CharsetDecoder decoder = Charset.forName("UTF-8").newDecoder();
 		in.position(0);
-		
+
 		LOGGER.info("【服务器解析】继续接受in：" + in.remaining());
 		IoBuffer bufTmp = null;
-		bufTmp = IoBuffer.allocate(
-				newIoBuffer.remaining() + in.remaining())
+		bufTmp = IoBuffer.allocate(newIoBuffer.remaining() + in.remaining())
 				.setAutoExpand(true);
 		bufTmp.order(newIoBuffer.order());
 		bufTmp.put(newIoBuffer);
 		bufTmp.put(in);
 		bufTmp.flip();
 		newIoBuffer = bufTmp;
-		
-		
-		// ByteFileMessage bfm = new ByteFileMessage();
+
 		if (isFirst) {
 			bfm.setSeq(newIoBuffer.getInt()); // 序号
 			bfm.setFileNameLength(newIoBuffer.getInt()); // 文件名长度（字节）
-			bfm.setFileName(newIoBuffer.getString(bfm.getFileNameLength(), decoder)); // 文件名。UTF-8格式
+			bfm.setFileName(newIoBuffer.getString(bfm.getFileNameLength(),
+					decoder)); // 文件名。UTF-8格式
 			bfm.setFileStreamLength(newIoBuffer.getInt()); // 文件长度（字节）
 			dataPool = bfm.getFileStreamLength();
 		}
 		// 如果读的文件的长度(字节：比如4096个字节)，远远大于这个缓冲区的容量（最大容量：2048字节），那么需要进行把一个个包黏起来
 		byte[] byteValue = null;
 		if (bfm.getFileStreamLength() > (newIoBuffer.remaining())) { // 2.14把limit改成remaining
-			/*
-			 * int dealDataLen = in.limit(); // 本次要处理的数据长度
-			 * 
-			 * byteValue = new byte[dealDataLen];
-			 */
 			bfm.setFileStreamLength(bfm.getFileStreamLength()
 					- (newIoBuffer.remaining())); // 2.14把limit改成remaining
-			
+
 			if (isFirst) { // 第一次
-				
 				isFirst = false;
-			/*} else {
-				LOGGER.info("【服务器解析】继续接受in：" + in.remaining());
-				bufTmp = IoBuffer.allocate(
-						newIoBuffer.remaining() + in.remaining())
-						.setAutoExpand(true);
-			}*/
-			
-			isFinish = false; // 数据未读取完。
-			LOGGER.info("【服务器解析】未解析数据：" + newIoBuffer.remaining() + "字节");
+				isFinish = false; // 数据未读取完。
+				LOGGER.info("【服务器解析】未解析数据：" + newIoBuffer.remaining() + "字节");
 			}
 		} else {
-			/*IoBuffer bufTmp = IoBuffer.allocate(
-					newIoBuffer.remaining() + in.remaining()).setAutoExpand(
-					true);
-			bufTmp.order(newIoBuffer.order());
-			bufTmp.put(newIoBuffer);*/
-			/*
-			 * byteValue = new byte[bfm.getFileStreamLength()];
-			 * bufTmp.put(in.get(byteValue,0,bfm.getFileStreamLength()));
-			 */
-			/*
-			 * byteValue = new byte[in.remaining()];
-			 * bufTmp.put(in.get(byteValue,0,byteValue.length));
-			 */
-			/*bufTmp.put(in);
-			bufTmp.flip();
-			newIoBuffer = bufTmp;*/
 			int remainingData = newIoBuffer.remaining(); // 总共多少
 			LOGGER.info("【服务器解析】当前数据池中总共多少：" + remainingData + " 字节");
 			if (remainingData > dataPool) {
@@ -140,8 +108,6 @@ public class ByteProtocalDecoder extends CumulativeProtocolDecoder {
 			isFinish = true;
 			LOGGER.info("【服务器解析】解析完成");
 		}
-		// LOGGER.info(new String(). bfm.getFileStream());
-		// return isFinish;
 	}
 
 	/**
